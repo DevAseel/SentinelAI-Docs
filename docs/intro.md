b861a3e689848d27aecda1f65c2b23eb73929444
DevAseel <dev.aseel@outlook.com>
2024-02-16 10:14:29 +0400

    init files

---
+++ docs/intro.md
@@ -0,0 +1,20 @@
+---
+sidebar_position: 1
+---
+
+# Installation
+
+Installation of SentinelAI is fairly simple and stright forward.
+
+## Clone Repository
+
+Get started by cloning the reposistory using the following command:
+
+```bash
+git clone https://github.com/DevAseel/SentinelAI.git
+```
+
+### What you'll need
+
+- [Python](https://www.python.org/downloads/) version 3.8 or above.
+
---
+++ docusaurus.config.ts
@@ -0,0 +1,120 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+const config: Config = {
+  title: "SentinelAI",
+  tagline: "Build Production Read RAG Apps with Confidence",
+  favicon: "img/favicon.ico",
+
+  // Set the production url of your site here
+  url: "https://sentinel.ai",
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: "/",
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: "", // Usually your GitHub org/user name.
+  projectName: "", // Usually your repo name.
+
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+
+  presets: [
+    [
+      "classic",
+      {
+        docs: {
+          sidebarPath: "./sidebars.ts",
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: "./src/css/custom.css",
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: "img/docusaurus-social-card.jpg",
+    navbar: {
+      title: "SentinelAI",
+      logo: {
+        alt: "SentinelAI Logo",
+        src: "img/logo.svg",
+      },
+      items: [
+        {
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Docs",
+        },
+        {
+          href: "https://github.com/DevAseel/SentinelAI",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Documentation",
+          items: [
+            {
+              label: "Getting Started",
+              to: "#",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "link 1",
+              href: "#",
+            },
+            {
+              label: "link 2",
+              href: "#",
+            },
+            {
+              label: "link 3",
+              href: "#",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "link 1",
+              to: "#",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} SentinelAI.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
